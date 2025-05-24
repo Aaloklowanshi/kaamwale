2a16d76dc2f1a572af30ea4f6df560acae0b5df2
Aaloklowanshi <lowanshiaalok@gmail.com>
2025-05-24 21:51:15 +0530

    how you doing

---
+++ demo/src/main/java/com/example/demo/dto/UserDto.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+}
